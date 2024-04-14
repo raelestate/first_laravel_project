4142160ef48ab41a22b97278fa53555747e6d57a
RaelEstate <rhyllneri@gmail.com>
2024-04-14 21:59:57 +0800

    hey

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="flex items-center justify-center">
+        <div>
+            <h1 class="text-xl">Nasaag ka. <a href="/">Balik sa Home!</a></h1>
+        </div>
+    </div>
+</x-app-layout>
